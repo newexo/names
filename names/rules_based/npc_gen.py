ff68d7825d1f07674a871b47662b50e43ec1ac8b
Reuben Brasher <rkbrasher@gmail.com>
2022-04-06 23:23:22 -0700

    Generate elf names

--- names/rules_based/npc_gen.py
+++ names/rules_based/npc_gen.py
@@ -1,34 +1,157 @@
+import re
 import pandas as pd
 
 from names import directories
+
 
 def npc_filename(filename):
     path = directories.data("npc")
     return directories.qualifyname(path, filename)
 
+
 def humans():
     with open(npc_filename("human_names.txt")) as f:
         return [line.strip() for line in f if len(line.strip())]
 
+
 def female_humans():
     hn = humans()
     return [line[4:] for line in hn if line[:4] == "[F]="]
+
 
 def male_humans():
     hn = humans()
     return [line[4:] for line in hn if line[:4] == "[M]="]
 
 
-def female_human_df():
+tag_regx = re.compile(r"\[([^\]]+)\]=?")
+
+
+def count_tags(line):
+    return len(tag_regx.findall(line))
+
+
+def find_tags(line):
+    return tag_regx.findall(line)
+
+
+def strip_tags(line):
+    return tag_regx.sub("", line.strip())
+
+
+def is_tagged(tag, line):
+    tags = find_tags(line)
+    return len(tags) == 1 and tags[0] == tag
+
+
+def find_tagged(tag, f):
+    return [strip_tags(line) for line in f if is_tagged(tag, line)]
+
+
+def is_female(line):
+    return is_tagged("F", line) or is_tagged("F'", line)
+
+
+def find_females(f):
+    return [strip_tags(line) for line in f if is_female(line)]
+
+
+def is_male(line):
+    return is_tagged("M", line) or is_tagged("M'", line)
+
+
+def find_males(f):
+    return [strip_tags(line) for line in f if is_female(line)]
+
+
+def filter_strip_file(filename, pred, should_capitalize=True):
+    def alter_line(line):
+        line = strip_tags(line)
+        if should_capitalize:
+            return line.capitalize()
+        else:
+            return line
+
+    with open(npc_filename(filename)) as f:
+        return [alter_line(line) for line in f if pred(line)]
+
+
+def gendered_from_file(race, using_male):
+    is_gendered = is_male if using_male else is_female
+    filename = "{}_names.txt".format(race)
+    return filter_strip_file(filename, is_gendered)
+
+
+def gendered_df(race, using_male, make_names=gendered_from_file):
+    word = "male" if using_male else "female"
+    letter = "M" if using_male else "F"
+    source = "npc {} {}".format(word, race)
     df = pd.DataFrame()
-    df['name'] = female_humans()
-    df['gender'] = "F"
-    df['source'] = "npc female human"
+    df["name"] = make_names(race, using_male)
+    df["gender"] = letter
+    df["source"] = source
     return df
 
+
+def female_human_df():
+    return gendered_df("human", False)
+
+
 def male_humans_df():
-    df = pd.DataFrame()
-    df['name'] = male_humans()
-    df['gender'] = "M"
-    df['source'] = "npc male human"
-    return df
+    return gendered_df("human", True)
+
+
+def female_gnome_df():
+    return gendered_df("gnome", False)
+
+
+def male_gnome_df():
+    return gendered_df("gnome", True)
+
+
+def female_halfling_df():
+    return gendered_df("halfling", False)
+
+
+def male_halfling_df():
+    return gendered_df("halfling", True)
+
+def list_elf_names(race, using_male):
+    found = gendered_from_file(race, using_male)
+    permuted = permuted_elf_names(using_male)
+    return found + permuted
+
+def female_elf_df():
+   return gendered_df("elf", False, list_elf_names)
+
+
+def male_elf_df():
+   return gendered_df("elf", True, list_elf_names)
+
+
+def name_parts(filename, part_names):
+    return [filter_strip_file(
+            filename, lambda line: is_tagged(part_name, line), should_capitalize=False
+        ) for part_name in part_names
+        ]
+
+
+def elf_part_names(is_male):
+    letter = "m" if is_male else "f"
+    part_names = ["pre", "in", "suf"]
+    gendered_part_names = [letter + part_name for part_name in part_names]
+    parts = name_parts("elf_names.txt", gendered_part_names)
+    return {part_name: part for part_name, part in zip(part_names, parts)}
+
+
+def permuted_elf_names(is_male):
+    parts = elf_part_names(is_male)
+    parts["in"].append("")
+    names = []
+    for pre in parts['pre']:
+        for mid in parts['in']:
+            for suf in parts['suf']:
+                name = pre + mid + suf
+                name = name.capitalize()
+                names.append(name)
+    return names
--- names/tests/test_npc.py
+++ names/tests/test_npc.py
@@ -5,7 +5,14 @@ from names.rules_based import npc_gen
 
 class TestNPC(unittest.TestCase):
     def setUp(self):
-        pass
+        self.lines = [
+            "",
+            "[S]=[M]",
+            "[M]=Abdallah\n",
+            "[mpre]=ivelli",
+            "[F]=[fpre][fin][fsuf]",
+            "[F']=Anastrianna\n",
+        ]
 
     def tearDown(self):
         pass
@@ -17,7 +24,7 @@ class TestNPC(unittest.TestCase):
         actual = list(df.columns)
         self.assertEqual(expected, actual)
         actual = df.iloc[100].to_dict()
-        expected = {'gender': 'F', 'name': 'Thora', 'source': 'npc female human'}
+        expected = {"gender": "F", "name": "Thora", "source": "npc female human"}
         self.assertEqual(expected, actual)
 
     def test_male_humans_df(self):
@@ -27,5 +34,174 @@ class TestNPC(unittest.TestCase):
         actual = list(df.columns)
         self.assertEqual(expected, actual)
         actual = df.iloc[100].to_dict()
-        expected = {'gender': 'M', 'name': 'Geirstein', 'source': 'npc male human'}
+        expected = {"gender": "M", "name": "Geirstein", "source": "npc male human"}
         self.assertEqual(expected, actual)
+
+    # def test_female_dwarf_df(self):
+    #     self.fail("incomplete")
+    #
+    # def test_male_dwarf_df(self):
+    #     self.fail("incomplete")
+
+    def test_female_elf_df(self):
+        df = npc_gen.female_elf_df()
+        self.assertEqual(10+ 10 * 10 * 9, df.name.count())
+        expected = ["name", "gender", "source"]
+        actual = list(df.columns)
+        self.assertEqual(expected, actual)
+        actual = df.iloc[5].to_dict()
+        expected = {'gender': 'F', 'name': 'Lia', 'source': 'npc female elf'}
+        self.assertEqual(expected, actual)
+
+        actual = df.iloc[10].to_dict()
+        expected = {'gender': 'F', 'name': 'Anastrianna', 'source': 'npc female elf'}
+        self.assertEqual(expected, actual)
+
+        actual = df.iloc[10 + 501].to_dict()
+        expected = {'gender': 'F', 'name': 'Liquiqui', 'source': 'npc female elf'}
+        self.assertEqual(expected, actual)
+
+    def test_male_elf_df(self):
+        df = npc_gen.male_elf_df()
+        self.assertEqual(10 + 10 * 4 * 9, df.name.count())
+        expected = ["name", "gender", "source"]
+        actual = list(df.columns)
+        self.assertEqual(expected, actual)
+        actual = df.iloc[5].to_dict()
+        expected = {'gender': 'M', 'name': 'Ivellios', 'source': 'npc male elf'}
+        self.assertEqual(expected, actual)
+
+        actual = df.iloc[10].to_dict()
+        expected = {'gender': 'M', 'name': 'Aravil', 'source': 'npc male elf'}
+        self.assertEqual(expected, actual)
+
+        actual = df.iloc[10 + 301].to_dict()
+        expected = {'gender': 'M', 'name': 'Thamimo', 'source': 'npc male elf'}
+        self.assertEqual(expected, actual)
+
+    def test_female_elf_permuted_names(self):
+        names = npc_gen.permuted_elf_names(False)
+        self.assertEqual(10 * 10 * 9, len(names))
+        self.assertEqual('Anastrianna', names[0])
+        self.assertEqual("Liquiqui", names[501])
+
+    def test_male_elf_permuted_names(self):
+        names = npc_gen.permuted_elf_names(True)
+        self.assertEqual(10 * 4 * 9, len(names))
+        self.assertEqual('Aravil', names[0])
+        self.assertEqual("Thamimo", names[301])
+
+    def test_female_gnome_df(self):
+        df = npc_gen.female_gnome_df()
+        self.assertEqual(10, df.name.count())
+        expected = ["name", "gender", "source"]
+        actual = list(df.columns)
+        self.assertEqual(expected, actual)
+        actual = df.iloc[5].to_dict()
+        expected = {'gender': 'F', 'name': 'Loopmottin', 'source': 'npc female gnome'}
+        self.assertEqual(expected, actual)
+
+    def test_male_gnome_df(self):
+        df = npc_gen.male_gnome_df()
+        self.assertEqual(10, df.name.count())
+        expected = ["name", "gender", "source"]
+        actual = list(df.columns)
+        self.assertEqual(expected, actual)
+        actual = df.iloc[5].to_dict()
+        expected = {'gender': 'M', 'name': 'Jebeddo', 'source': 'npc male gnome'}
+        self.assertEqual(expected, actual)
+
+    def test_female_halfling_df(self):
+        df = npc_gen.female_halfling_df()
+        self.assertEqual(34, df.name.count())
+        expected = ["name", "gender", "source"]
+        actual = list(df.columns)
+        self.assertEqual(expected, actual)
+        actual = df.iloc[5].to_dict()
+        expected = {'gender': 'F', 'name': 'Lavinia', 'source': 'npc female halfling'}
+        self.assertEqual(expected, actual)
+
+    def test_male_halfling_df(self):
+        df = npc_gen.male_halfling_df()
+        self.assertEqual(20, df.name.count())
+        expected = ["name", "gender", "source"]
+        actual = list(df.columns)
+        self.assertEqual(expected, actual)
+        actual = df.iloc[5].to_dict()
+        expected = {'gender': 'M', 'name': 'Lyle', 'source': 'npc male halfling'}
+        self.assertEqual(expected, actual)
+
+    # def test_female_monster_df(self):
+    #     self.fail("incomplete")
+    #
+    # def test_male_monster_df(self):
+    #     self.fail("incomplete")
+
+    def test_count_tags(self):
+        expected = [0, 2, 1, 1, 4, 1]
+        actual = [npc_gen.count_tags(line) for line in self.lines]
+        self.assertEqual(expected, actual)
+
+    def test_find_tags(self):
+        expected = [
+            [],
+            ["S", "M"],
+            ["M"],
+            ["mpre"],
+            ["F", "fpre", "fin", "fsuf"],
+            ["F'"],
+        ]
+        actual = [npc_gen.find_tags(line) for line in self.lines]
+        self.assertEqual(expected, actual)
+
+    def test_strip_tags(self):
+        expected = ["", "", "Abdallah", "ivelli", "", "Anastrianna"]
+        actual = [npc_gen.strip_tags(line) for line in self.lines]
+        self.assertEqual(expected, actual)
+
+    def test_is_tagged(self):
+        self.assertTrue(npc_gen.is_tagged("foo", "[foo]=adfgzdfgzdfg"))
+        self.assertFalse(npc_gen.is_tagged("foo", "[foo]=[bar]"))
+        self.assertFalse(npc_gen.is_tagged("foo", "[bla]=[foo]"))
+        self.assertFalse(npc_gen.is_tagged("foo", "[F]=adfgzdfgzdfg"))
+        self.assertFalse(npc_gen.is_tagged("foo", ""))
+
+    def test_is_male(self):
+        self.assertTrue(npc_gen.is_male("[M]=adfgzdfgzdfg"))
+        self.assertTrue(npc_gen.is_male("[M']=adfgzdfgzdfg"))
+        self.assertFalse(npc_gen.is_male("[foo]=[bar]"))
+        self.assertFalse(npc_gen.is_male("[bla]=[foo]"))
+        self.assertFalse(npc_gen.is_male("[F]=adfgzdfgzdfg"))
+        self.assertFalse(npc_gen.is_male(""))
+
+    def test_is_female(self):
+        self.assertTrue(npc_gen.is_female("[F]=adfgzdfgzdfg"))
+        self.assertTrue(npc_gen.is_female("[F']=adfgzdfgzdfg"))
+        self.assertFalse(npc_gen.is_female("[foo]=[bar]"))
+        self.assertFalse(npc_gen.is_female("[bla]=[foo]"))
+        self.assertFalse(npc_gen.is_female("[M]=adfgzdfgzdfg"))
+        self.assertFalse(npc_gen.is_female(""))
+
+    def test_female_elf_partnames(self):
+        partnames = npc_gen.elf_part_names(False)
+        self.assertEqual(3, len(partnames))
+        pre = partnames['pre']
+        mid = partnames['in']
+        suf = partnames['suf']
+        self.assertEqual(10, len(pre))
+        self.assertEqual(9, len(mid))
+        self.assertEqual(9, len(suf))
+        expected = {'pre': ['ana', 'anti', 'drusi', 'felo', 'iele', 'li', 'qilla', 'sila', 'vala', 'xana'], 'in': ['stria', 'nu', 'si', 'ni', 'la', 'qui', 'qua', 'nthe', 'phi'], 'suf': ['nna', 'nua', 'lia', 'sial', 'nia', 'the', 'qui', 'nthe', 'phia']}
+        self.assertEqual(expected, partnames)
+
+    def test_male_elf_partnames(self):
+        partnames = npc_gen.elf_part_names(True)
+        self.assertEqual(3, len(partnames))
+        pre = partnames['pre']
+        mid = partnames['in']
+        suf = partnames['suf']
+        self.assertEqual(10, len(pre))
+        self.assertEqual(3, len(mid))
+        self.assertEqual(9, len(suf))
+        expected = {'pre': ['ara', 'a', 'enia', 'he', 'himo', 'ivelli', 'lauci', 'quari', 'thami', 'thari'], 'in': ['v', 'm', 'l'], 'suf': ['il', 'ust', 'is', 'ian', 'o', 'os', 'on', 'or', 'ol']}
+        self.assertEqual(expected, partnames)
